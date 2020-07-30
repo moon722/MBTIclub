196682d18d5e5d9afb9c4605cdd3fcfcd35be891
kdh7575070 <kdh7575070@naver.com>
2020-07-30 16:38:42 +0900

    Taeha

---
+++ app/src/main/java/com/example/mbticlub/VPAdapter.java
@@ -0,0 +1,40 @@
+package com.example.mbticlub;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class VPAdapter extends FragmentPagerAdapter{
+
+    private ArrayList<Fragment> items;
+    private ArrayList<String> itext = new ArrayList<String>();
+
+    public VPAdapter(FragmentManager fm) {
+        super(fm);
+        items = new ArrayList<Fragment>();
+        items.add(new BlankFragment());
+        items.add(new BlankFragment2());
+
+        itext.add("Notifications");
+        itext.add("Message");
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return itext.get(position);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+}
