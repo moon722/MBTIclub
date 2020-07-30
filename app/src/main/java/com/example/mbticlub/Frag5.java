a106f3e8d319b6c4c7cc9017717721ab92667afd
Sangjin <eu2525@khu.ac.kr>
2020-07-30 16:30:50 +0900

    한상진 profile.png back.xml Frag5.java farg5.xml

--- app/src/main/java/com/example/mbticlub/Frag5.java
+++ app/src/main/java/com/example/mbticlub/Frag5.java
@@ -1,23 +1,51 @@
 package com.example.mbticlub;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.mbticlub.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Frag5 extends Fragment {
 
+    private ListView person_list;
     private View view;
 
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.frag5,container,false);
+        view = inflater.inflate(R.layout.frag5, container, false);
 
+//        MBTI_selection = (Button)view.findViewById(R.id.MBTI_selection);
+//        board_title = (TextView)view.findViewById(R.id.board_title);
+
+
+        person_list = (ListView) view.findViewById(R.id.person_list);
+
+        List<String> person_data = new ArrayList<>();//arraylist 배열 안에 스트링 형태로 리스트를 만들겠다
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, person_data);
+        person_list.setAdapter(adapter);
+
+        person_data.add("내 정보 관리");
+        person_data.add("구독 정보");
+        person_data.add("코인 충전");
+
+        adapter.notifyDataSetChanged();
         return view;
     }
 }
