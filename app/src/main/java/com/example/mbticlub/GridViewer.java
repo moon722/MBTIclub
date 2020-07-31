0dff92df8c25861243efa8a60bad8dbcb665d1fb
hyelmy <herym3567@gmail.com>
2020-07-31 14:30:07 +0900

    권혜림 - 프레그먼트2 화면 업뎃

---
+++ app/src/main/java/com/example/mbticlub/GridViewer.java
@@ -0,0 +1,41 @@
+package com.example.mbticlub;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.example.mbticlub.R;
+
+public class GridViewer extends LinearLayout {
+
+    TextView textView;
+    ImageView imageView;
+    public GridViewer(Context context) {
+        super(context);
+
+        init(context);
+    }
+
+    public GridViewer(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context);
+    }
+
+    public void init(Context context){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.gridview,this,true);
+
+        textView = (TextView)findViewById(R.id.gridtext);
+        imageView = (ImageView) findViewById(R.id.gridimg);
+    }
+
+    public void setItem(GridItem singerItem){
+        textView.setText(singerItem.getName());
+        imageView.setImageResource(singerItem.getImage());
+    }
+}
