c40d0cd70de45a14f9aa3243985e2fcd1c600225
moon722 <miz722@khu.ac.kr>
2020-08-01 20:29:57 +0900

    인주 게시판 검색기능 구현

--- app/src/main/java/com/example/mbticlub/Frag3.java
+++ app/src/main/java/com/example/mbticlub/Frag3.java
@@ -3,6 +3,8 @@ package com.example.mbticlub;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +22,7 @@ import androidx.fragment.app.Fragment;
 import com.example.mbticlub.R;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Frag3 extends Fragment{
@@ -27,55 +30,53 @@ public class Frag3 extends Fragment{
 
     private View view;
     private ListView board_list;
-//    Button MBTI_selection;
+    //    Button MBTI_selection;
 //    TextView board_title;
     final CharSequence[] MBTIs = {"INTP","ENTP","INFP","ENFP","ESFP","ISFP"};
 //    AlertDialog.Builder builder = new AlertDialog.Builder(null);
 
+//    -------------------
+//    -------------------
+
+//    -------------------
+    String[] board_items;
+    ArrayList<String> board_listitems;
+    ArrayAdapter<String> board_adapter;
+    ListView board_listview;
+    EditText board_editText;
+//    -------------------
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag3,container,false);
+        board_listview = (ListView)view.findViewById(R.id.board_listview);
+        board_editText=(EditText)view.findViewById(R.id.textSearch);
+        init_boardList();
+        board_editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-//        MBTI_selection = (Button)view.findViewById(R.id.MBTI_selection);
-//        board_title = (TextView)view.findViewById(R.id.board_title);
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().equals("")){
+                    //reset listview
+                    init_boardList();
+                }
+                else{
+                    //perform search
+                    searchItem(s.toString());
+                }
+            }
 
+            @Override
+            public void afterTextChanged(Editable s) {
 
-        board_list = (ListView) view.findViewById(R.id.board_list);
-
-        List<String> board_data = new ArrayList<>();//arraylist 배열 안에 스트링 형태로 리스트를 만들겠다
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),android.R.layout.simple_list_item_1,board_data);
-        board_list.setAdapter(adapter);
-
-        board_data.add("여행");
-        board_data.add("영화");
-        board_data.add("질문과답변");
-        board_data.add("연애");
-        board_data.add("음악");
-        board_data.add("운동");
-        board_data.add("학업");
-        board_data.add("소모임");
-        board_data.add("자유");
-        board_data.add("독서");
-        board_data.add("독서");
-        board_data.add("독서");
-        board_data.add("독서");
-        board_data.add("독서");
-        board_data.add("독서");
-        board_data.add("독서");
-        board_data.add("독서");
-        board_data.add("독서");
-
-
-
-
-
-
-
-        adapter.notifyDataSetChanged();//이 상태를 저장
+            }
+        });
 
         //add data to listview
         ListView listView=new ListView(this.getActivity());
@@ -130,6 +131,21 @@ public class Frag3 extends Fragment{
 
         return view;
     }
+    public void searchItem(String textToSearch){
+        for(String item:board_items){
+            if(!item.contains(textToSearch)){
+                board_listitems.remove(item);
+            }
+
+            board_adapter.notifyDataSetChanged();
+        }
+
+    }
+    public void init_boardList(){
+        board_items=new String[]{"Full board","travel","date","game","exercise","cooking"};
+        board_listitems=new ArrayList<>(Arrays.asList(board_items));
+        board_adapter=new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,board_listitems);
+        board_listview.setAdapter(board_adapter);
+    }
 
 }
-
