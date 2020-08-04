c739e73da1abd7cad9aff316b41cd5e1a57232c1
moon722 <miz722@khu.ac.kr>
2020-08-04 15:28:44 +0900

    인주 PostRequest:생성

---
+++ app/src/main/java/com/example/mbticlub/PostResister.java
@@ -0,0 +1,15 @@
+package com.example.mbticlub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostResister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_resister);
+    }
+
+}
