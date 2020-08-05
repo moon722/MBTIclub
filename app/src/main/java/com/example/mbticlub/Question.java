742e62bb1b05ea8f9af269abb7c7f562fcb92852
hyelmy <herym3567@gmail.com>
2020-08-05 15:43:43 +0900

    권혜림 - ox 취향테스트 누르면 다음 db 목록 로 넘어가기

---
+++ app/src/main/java/com/example/mbticlub/Question.java
@@ -0,0 +1,26 @@
+package com.example.mbticlub;
+
+public class Question {
+    int id;
+    String title;
+    int ox;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title=title;
+    }
+    public int getOx(){
+        return ox;
+    }
+    public void setOx(int ox){
+        this.ox= ox;
+    }
+
+}
